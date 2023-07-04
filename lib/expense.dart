aceb4f65043bdc481f580ca1290f125b507e1ae0
Alen Boric <boricalen@live.com>
2023-07-04 22:30:14 +0200

    enums,dummy data,ListView.builder

--- lib/expense.dart
+++ lib/expense.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/expense.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({Key? key}) : super(key: key);
 
@@ -8,13 +10,27 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
         children: [
           Text("The chart"),
-          Text("Expenses list ..."),
         ],
       ),
     );
